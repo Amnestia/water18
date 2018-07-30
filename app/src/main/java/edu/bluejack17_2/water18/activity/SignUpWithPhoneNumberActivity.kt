d14f21ac7b5a5bbf2834b47fcfaedaecfd33a683
amnestia <goldenleon5632@gmail.com>
2018-07-30 15:37:59 +0700

    push push push push push

---
+++ app/src/main/java/edu/bluejack17_2/water18/activity/SignUpWithPhoneNumberActivity.kt
@@ -0,0 +1,16 @@
+package edu.bluejack17_2.water18.activity
+
+import android.app.Activity
+import android.os.Bundle
+import edu.bluejack17_2.water18.R
+
+class SignUpWithPhoneNumberActivity : Activity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_sign_up_with_phone_number)
+    }
+}
