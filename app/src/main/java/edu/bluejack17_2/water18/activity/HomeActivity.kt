89ac187f1c13e747b9561e423589e2698a476647
amnestia <goldenleon5632@gmail.com>
2018-08-01 13:52:45 +0700

    push push push push push push push push push push push push

--- app/src/main/java/edu/bluejack17_2/water18/activity/HomeActivity.kt
+++ app/src/main/java/edu/bluejack17_2/water18/activity/HomeActivity.kt
@@ -2,6 +2,8 @@ package edu.bluejack17_2.water18.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
 import edu.bluejack17_2.water18.R
 
 class HomeActivity : Activity()
@@ -11,5 +13,12 @@ class HomeActivity : Activity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var layout:DrawerLayout=findViewById(R.id.drawer_layout)
+        val view:NavigationView=findViewById(R.id.navigation_view)
+        view.setNavigationItemSelectedListener { item ->
+            item.isChecked
+        }
+
     }
 }
