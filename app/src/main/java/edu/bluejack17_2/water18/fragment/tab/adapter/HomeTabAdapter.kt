07ba34ee53869142647ddab4feace96938515dcc
amnestia <goldenleon5632@gmail.com>
2018-08-08 09:07:25 +0700

    push

--- app/src/main/java/edu/bluejack17_2/water18/fragment/tab/adapter/HomeTabAdapter.kt
+++ app/src/main/java/edu/bluejack17_2/water18/fragment/tab/adapter/HomeTabAdapter.kt
@@ -3,9 +3,10 @@ package edu.bluejack17_2.water18.fragment.tab.adapter
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
-import edu.bluejack17_2.water18.fragment.CartFragment
+import edu.bluejack17_2.water18.fragment.HistoryFragment
+import edu.bluejack17_2.water18.fragment.HomeFragment
 
-class TabAdapter(private val fm: FragmentManager, private val tabsCount: Int) : FragmentStatePagerAdapter(fm)
+class HomeTabAdapter(private val fm: FragmentManager, private val tabsCount: Int) : FragmentStatePagerAdapter(fm)
 {
     override fun getCount(): Int
     {
@@ -16,7 +17,8 @@ class TabAdapter(private val fm: FragmentManager, private val tabsCount: Int) : 
     {
         return when(position)
         {
-            0->CartFragment.newInstance()
+            0-> HomeFragment.newInstance()
+            1-> HistoryFragment.newInstance()
             else -> null
         }
     }
