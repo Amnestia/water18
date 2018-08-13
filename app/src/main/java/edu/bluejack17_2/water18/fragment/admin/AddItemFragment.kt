51d5e3760319a56e89672dce8edc054668c013b0
amnestia <goldenleon5632@gmail.com>
2018-08-13 14:25:17 +0700

    push

--- app/src/main/java/edu/bluejack17_2/water18/fragment/admin/AddItemFragment.kt
+++ app/src/main/java/edu/bluejack17_2/water18/fragment/admin/AddItemFragment.kt
@@ -30,7 +30,6 @@ class AddItemFragment : Fragment(), View.OnClickListener
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-        // Inflate the layout for this fragment
         addListener()
         return inflater.inflate(R.layout.fragment_add_item, container, false)
     }
