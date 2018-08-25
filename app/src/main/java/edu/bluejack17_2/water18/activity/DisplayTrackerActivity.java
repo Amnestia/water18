e39e92d80d9c6fbf986f35c43c8a4e6407e968a6
Subco <yesunutomo2@gmail.com>
2018-08-25 10:50:22 -0700

    YS - Display Tracker (Done Need to be tested)

--- app/src/main/java/edu/bluejack17_2/water18/activity/DisplayTrackerActivity.java
+++ app/src/main/java/edu/bluejack17_2/water18/activity/DisplayTrackerActivity.java
@@ -1,16 +1,22 @@
 package edu.bluejack17_2.water18.activity;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -22,7 +28,7 @@ public class DisplayTrackerActivity extends FragmentActivity implements OnMapRea
 
 
     private static final String TAG = DisplayTrackerActivity.class.getSimpleName();
-    private HashMap<String, Marker> markerHashMap = new HashMap<>();
+    private HashMap<String, Marker> mMarker = new HashMap<>();
     private GoogleMap mMap;
 
     @Override
@@ -49,18 +55,64 @@ public class DisplayTrackerActivity extends FragmentActivity implements OnMapRea
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMaxZoomPreference(16);
+        subscribeToUpdates();
         // Add a marker in Sydney and move the camera
 //        LatLng sydney = new LatLng(-34, 151);
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
+
+    //this function reads for updated location of the driver
+    //if updated, ths function will call setMarker()
     private void subscribeToUpdates(){
+        DatabaseReference ref = FirebaseDatabase .getInstance().getReference("driver_location");
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                setMarker(dataSnapshot);
+            }
 
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                setMarker(dataSnapshot);
+            }
 
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "failed to read value", databaseError.toException());
+            }
+        });
     }
 
+    //this function accepts the location from subscribeToUpdate
+    //then it will update the display on the map
     private void setMarker(DataSnapshot dataSnapshot){
-
+        String key = dataSnapshot.getKey();
+        HashMap<String, Object> value = (HashMap<String, Object>) dataSnapshot.getValue();
+        double lat = Double.parseDouble(value.get("latitude").toString());
+        double lng = Double.parseDouble(value.get("longitude").toString());
+        LatLng location = new LatLng(lat, lng);
+        if(!mMarker.containsKey(key)){
+            mMarker.put(key, mMap.addMarker(new MarkerOptions().title(key).position(location)));
+        }
+        else {
+            mMarker.get(key).setPosition(location);
+        }
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for(Marker marker : mMarker.values()){
+            builder.include(marker.getPosition());
+        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 300));
     }
 }
---
+++ app/src/main/java/edu/bluejack17_2/water18/service/TrackerService.java
@@ -0,0 +1,93 @@
+package edu.bluejack17_2.water18.service;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import edu.bluejack17_2.water18.R;
+
+public class TrackerService extends Service {
+
+    private static final String TAG = TrackerService.class.getSimpleName();
+
+    public TrackerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return  null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        buildNotification();
+        requestLocationUpdates();
+    }
+
+    private void buildNotification() {
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+        // Create the persistent notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Tracking, tap to cancel")
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.ic_location);
+        startForeground(1, builder.build());
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            unregisterReceiver(stopReceiver);
+            stopSelf();
+        }
+    };
+
+    private void requestLocationUpdates() {
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        final String path = "driver_location" + "/" + "driver1";
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(permission == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.requestLocationUpdates(request, new LocationCallback(){
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+                    Location location = locationResult.getLastLocation();
+                    if(location!=null){
+                        ref.setValue(location);
+                    }
+                }
+            }, null);
+        }
+    }
+
+
+}
